ab40249bc4e21ddcacd3ac93af42d1e16898f13d
juangod1 <juan.godfrid@gmail.com>
2020-08-17 13:05:00 -0300

    update script

--- bairesCatcher/load_baires_catcher.sql
+++ bairesCatcher/load_baires_catcher.sql
@@ -1,62 +1,58 @@
 CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS mobilitydb;
 
-DROP TABLE IF EXISTS metadata CASCADE;
 DROP TABLE IF EXISTS positions CASCADE;
 
-CREATE TABLE IF NOT EXISTS metadata (
-  id bigint,
-  route_id text,
-  agency_id int,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE IF NOT EXISTS positions (
-  id bigint,
+  trip_id text,
+  vehicle_id text NOT NULL,
   instant bigint NOT NULL,
-  stop_geom geometry('POINT', 4326),
   latitude float NOT NULL,
   longitude float NOT NULL,
-  PRIMARY KEY (id, instant),
-  CONSTRAINT fk_metadata
-    FOREIGN KEY(id)
-    REFERENCES metadata(id)
+  startdate text,
+  starttime text,
+  directionId int,
+  PRIMARY KEY (trip_id, vehicle_id, instant)
 );
 
-COPY metadata(id, route_id, agency_id) from :metadata DELIMITER ',' CSV HEADER;
-COPY positions(id, instant, latitude, longitude) from :positions DELIMITER ',' CSV HEADER;
+COPY positions(
+  trip_id,
+  vehicle_id,
+  instant,
+  latitude,
+  longitude,
+  startdate,
+  starttime,
+  directionId) from :positions DELIMITER ',' CSV HEADER;
 
-\! echo '...Creating trip_input'
-DROP TABLE IF EXISTS trips_input;
-CREATE TABLE trips_input (
-    trip_id text,
-	route_id text,
-	agency_id text,
-	date timestamp,
-	point_geom geometry
-);
+\! echo '...Altering positions'
+ALTER TABLE positions ADD COLUMN point geometry;
+UPDATE positions
+SET point = ST_GeomFromText('POINT('|| longitude ||' '|| latitude ||')');
 
-\! echo '...Inserting trip_input'
-INSERT INTO trips_input
-SELECT p.id, route_id, agency_id,
-	to_timestamp(instant) as timestamp, ST_GeomFromText('POINT('|| p.longitude ||' '|| p.latitude ||')') as point_geom
-FROM positions p JOIN metadata m ON m.id = p.id;
 
 \! echo '...Creating trip_mdb'
 DROP TABLE IF EXISTS trips_mdb;
 CREATE TABLE trips_mdb (
-	trip_id text NOT NULL,
-	agency_id text NOT NULL,
-	route_id text NOT NULL,
+    trip_id text NOT NULL,
+    vehicle_id text NOT NULL,
+	startdate text,
+    starttime text,
 	trip tgeompoint,
-	PRIMARY KEY (trip_id)
+	PRIMARY KEY (trip_id, vehicle_id, startdate, starttime)
 );
 
 \! echo '...Inserting trip_mdb'
-INSERT INTO trips_mdb(trip_id, agency_id, route_id, trip)
-SELECT trip_id, agency_id, route_id, tgeompointseq(array_agg(tgeompointinst(point_geom, date) ORDER BY date))
-FROM trips_input
-GROUP BY trip_id, agency_id, route_id;
+INSERT INTO trips_mdb(
+    trip_id,
+    vehicle_id,
+	startdate,
+    starttime,
+	trip)
+SELECT trip_id, vehicle_id, startdate, starttime,
+ tgeompointseq(array_agg(tgeompointinst(point, to_timestamp(instant)) ORDER BY instant))
+FROM positions
+GROUP BY trip_id, vehicle_id, starttime, startdate;
 
 \! echo '...Updating trip_mdb'
 ALTER TABLE trips_mdb ADD COLUMN traj geometry;
