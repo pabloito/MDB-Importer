51f76a08e8a276fcd67a6d3aaaea52e9a0132ff7
juangod1 <juan.godfrid@gmail.com>
2020-09-20 13:55:29 -0300

    Fix for random decimals pero esta comentado porque hace que tarde banda.

--- bairesCatcher/invertCoordinates.py
+++ bairesCatcher/invertCoordinates.py
@@ -1,6 +1,6 @@
 import pandas as pd
-#/home/juangod/Documents/positions.csv'
-reader = pd.read_csv(r'./positions.csv',chunksize=10000)
+
+reader = pd.read_csv(r'/home/juangod/Documents/positions.csv', chunksize=10000)
 
 output_path = 'positions_coordinates_inverted.csv'
 
@@ -10,8 +10,11 @@ for i, chunk in enumerate(reader):
     for idx, val in enumerate(chunk["latitude"]):
         if (val > 0):
             chunk["latitude"][i*10000+idx] = chunk["latitude"][i*10000+idx]*-1
+        #chunk["latitude"][i*10000+idx] = round(chunk["latitude"][i*10000+idx], 5)
+
     for idx, val in enumerate(chunk["longitude"]):
         if (val > 0):
             chunk["longitude"][i*10000+idx] = chunk["longitude"][i*10000+idx]*-1
+        #chunk["longitude"][i*10000+idx] = round(chunk["longitude"][i*10000+idx], 5)
     chunk.to_csv(output_path, index=False, header=header_flag, mode='a')
     header_flag = False
