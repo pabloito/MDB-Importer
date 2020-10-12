5afe9a4414bd8b966dab8209e4b242c944cd40a8
pabloito <pradnic@itba.edu.ar>
2020-10-12 11:29:05 -0300

    grafana queries

--- bairesCatcher/grafana.sql
+++ bairesCatcher/grafana.sql
@@ -1,38 +1,35 @@
 -- Average speed by starting hour (ST)
-SELECT AVG(twavg(speed(Trip))) as value, date_trunc('hour',startTimestamp(Trip)) as time
+SELECT AVG(twavg(speed(Trip))) as value, date_trunc('hour',startTimestamp(Trip)) at time zone 'America/Argentina/Buenos_Aires' as time
 FROM Trips_mdb
 group by time
 order by time;
 
 --Average speed by starting hour (RT)
-SELECT AVG(twavg(speed(Trip))) as value, date_trunc('hour', starttimefull) as time
+SELECT AVG(twavg(speed(Trip))) as value, date_trunc('hour', starttimefull) at time zone 'America/Argentina/Buenos_Aires' as time
 FROM Trips_mdbrt
 WHERE starttimefull is not null
 group by time
 order by time;
 
--- Average speed by starting hour (ST, RT lines)
-SELECT AVG(twavg(speed(Trip))) as value, date_trunc('hour',startTimestamp(Trip)) as time
-FROM Trips_mdb t1
-WHERE EXISTS (
-	SELECT *
-	FROM Trips_mdbrt t2
-	WHERE t1.trip_id = t2.trip_id		
-)
-group by time
-order by time;
-
 -- Average speed by starting day (ST)
-SELECT AVG(twavg(speed(Trip))) as value, date_trunc('day',startTimestamp(Trip)) as time
+SELECT AVG(twavg(speed(Trip))) as value, date_trunc('day',startTimestamp(Trip)) at time zone 'America/Argentina/Buenos_Aires' as time
 FROM Trips_mdb
 group by time
 order by time;
 
 --Average speed by starting day (RT)
-SELECT AVG(twavg(speed(Trip))) as value, date_trunc('day', starttimefull) as time
+SELECT AVG(twavg(speed(Trip))) as value, date_trunc('day', starttimefull) at time zone 'America/Argentina/Buenos_Aires' as time
 FROM Trips_mdbrt
 WHERE starttimefull is not null
 group by time
 order by time;
 
 
+SELECT
+  'Aug 23 13:43:07 America/Argentina/Buenos_Aires 2020'::timestamp with time zone as time,
+  100 as value,
+  s.stop_lat as latitude,
+  s.stop_lon as longitude,
+  s.stop_id as name
+FROM stops s inner join stop_times st on st.stop_id = s.stop_id
+limit 100;
