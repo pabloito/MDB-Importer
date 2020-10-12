8729729ab58cbf90bf25a531159c697a71e4f501
pabloito <pradnic@itba.edu.ar>
2020-10-12 16:14:29 -0300

    updatesrid

--- bairesCatcher/grafana.sql
+++ bairesCatcher/grafana.sql
@@ -1,35 +1,25 @@
 -- Average speed by starting hour (ST)
-SELECT AVG(twavg(speed(Trip))) as value, date_trunc('hour',startTimestamp(Trip)) at time zone 'America/Argentina/Buenos_Aires' as time
+SELECT AVG(twavg(speed(Trip))) as static, date_trunc('hour',startTimestamp(Trip)) at time zone 'GMT-3' as time
 FROM Trips_mdb
 group by time
 order by time;
 
 --Average speed by starting hour (RT)
-SELECT AVG(twavg(speed(Trip))) as value, date_trunc('hour', starttimefull) at time zone 'America/Argentina/Buenos_Aires' as time
+SELECT AVG(twavg(speed(Trip))) as realtime, date_trunc('hour', starttimefull) at time zone 'America/Argentina/Buenos_Aires' as time
 FROM Trips_mdbrt
 WHERE starttimefull is not null
 group by time
 order by time;
 
 -- Average speed by starting day (ST)
-SELECT AVG(twavg(speed(Trip))) as value, date_trunc('day',startTimestamp(Trip)) at time zone 'America/Argentina/Buenos_Aires' as time
+SELECT AVG(twavg(speed(Trip))) as static, date_trunc('day',startTimestamp(Trip)) at time zone 'GMT-3' as time
 FROM Trips_mdb
 group by time
 order by time;
 
 --Average speed by starting day (RT)
-SELECT AVG(twavg(speed(Trip))) as value, date_trunc('day', starttimefull) at time zone 'America/Argentina/Buenos_Aires' as time
+SELECT AVG(twavg(speed(Trip))) as realtime, date_trunc('day', starttimefull) at time zone 'America/Argentina/Buenos_Aires' as time
 FROM Trips_mdbrt
 WHERE starttimefull is not null
 group by time
 order by time;
-
-
-SELECT
-  'Aug 23 13:43:07 America/Argentina/Buenos_Aires 2020'::timestamp with time zone as time,
-  100 as value,
-  s.stop_lat as latitude,
-  s.stop_lon as longitude,
-  s.stop_id as name
-FROM stops s inner join stop_times st on st.stop_id = s.stop_id
-limit 100;
---
+++ updateSRID.sql
@@ -0,0 +1,5 @@
+UPDATE trips_mdb
+SET trip = transform(trip, 5345);
+
+UPDATE trips_mdbrt
+SET trip = transform(trip, 5345);
